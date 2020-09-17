2d4fa25903c4039a08ffc57db0fb9f876bea996b
Miguel Rodriguez <daw011.2020@gmail.com>
2020-09-17 20:22:37 +0200

    Añadido el log

--- app/src/main/java/jesuitas/dam/primera/PrincipalActivity.java
+++ app/src/main/java/jesuitas/dam/primera/PrincipalActivity.java
@@ -1,14 +1,16 @@
 package jesuitas.dam.primera;
 
+import android.os.Bundle;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
+public class PrincipalActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("PrincipalActivity", "Feliz Cumpleaños Elena!");
     }
 }
